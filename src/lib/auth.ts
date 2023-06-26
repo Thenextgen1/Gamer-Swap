c566fff64f3e2f1d817fc67b5ac1dcc972aa80df
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-06-26 18:32:16 +0100

    updated auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,13 +9,14 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     CredentialsProvider({
-      name: "Sign in",
+      name: "Email",
       credentials: {
-        email: {
+        username: {
           label: "Email",
           type: "email",
           placeholder: "example@example.com",
         },
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         const user = { id: "1", name: "Admin", email: "admin@admin.com" };
