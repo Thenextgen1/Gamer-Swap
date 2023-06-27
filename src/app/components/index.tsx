cafb6ef3204809df30744f596f981017835668be
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-06-27 04:42:24 +0100

    moved to lib folder

--- src/app/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "./Layout/Header";
-export { default as Footer } from "./Layout/Footer";
-export { default as DesktopNav } from "./Layout/Menus/DesktopNav";
-export { default as MobileNav } from "./Layout/Menus/MobileNav";
