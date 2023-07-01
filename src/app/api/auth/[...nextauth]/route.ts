d120823455be17eabac5e730905fdeb298a3e815
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-07-01 04:31:22 +0100

    moved auth options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/lib/models/auth";
+import { authOptions } from "@/app/lib/utils/auth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
