99659e77ff56355f7e283641994a997da1e1fd65
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-07-01 04:37:30 +0100

    added regsiter and login pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,13 @@
+import { Metadata } from "next";
 import Form from "../lib/components/Login/Form";
-import { poppins } from "@/app/page";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
 
 export default function Login() {
   return (
     <main className="max-w-sm mx-auto">
-      <h2
-        className={`leading-[1.15] mt-12 mx-auto  pl-2 sm:pl-0 text-xl my-6 sm:text-2xl font-semibold font-sans ${poppins.variable}`}
-      >
-        Log In
-      </h2>
       <section>
         <Form />
       </section>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,21 @@
 import { Metadata } from "next";
+
 import Form from "../lib/components/Register/Form";
+import { poppins } from "../lib/utils/font";
 
 export const metadata: Metadata = {
-  title: "Gamer Swap",
+  title: "Gamer Swap | Register",
 };
 
 const Register = () => {
   return (
     <main className="mt-28">
-      <section className="mx-auto max-w-7xl">
+      <section className="mx-auto max-w-sm">
+        <h2
+          className={`leading-[1.15] text-xl sm:text-2xl font-semibold font-sans px-2 ${poppins.variable}`}
+        >
+          Create Personal Account
+        </h2>
         <Form />
       </section>
     </main>
