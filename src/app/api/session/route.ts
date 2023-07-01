507d28d162da5e222dfb1141b776dd813aff4955
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-07-01 04:31:49 +0100

    added api route

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,12 +1,16 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/lib/models/auth";
+import { authOptions } from "@/app/lib/utils/auth";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
 
+  if (!session) {
+    return NextResponse.json({ message: "You are not logged in" });
+  }
+
   return NextResponse.json({
-    authenticated: !!session,
-    session,
+    name: session.user?.name,
+    email: session.user?.email,
   });
 }
