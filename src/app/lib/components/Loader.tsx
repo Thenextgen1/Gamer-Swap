dfb8615d11590d56c1d5cf7c7f0532c81f6710a9
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-07-01 04:38:32 +0100

    built loader

---
+++ src/app/lib/components/Loader.tsx
@@ -0,0 +1,11 @@
+import styles from "@/app/styles/Loader.module.css";
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center absolute top-1/2 left-0 right-0">
+      <div className={styles.custom_loader}></div>
+    </div>
+  );
+};
+
+export default Loader;
