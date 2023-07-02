e279cb32e64a643b098a5446f21ddd8a24a68c64
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-07-02 18:13:31 +0100

    moved form component to respective pages

---
+++ src/app/login/Form.tsx
@@ -0,0 +1,151 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import styles from "./Form.module.css";
+import Link from "next/link";
+import GoogleButton from "../lib/components/Buttons/GoogleButton";
+import FacebookButton from "../lib/components/Buttons/FacebookButton";
+import { signIn, useSession } from "next-auth/react";
+import { BsArrowRightCircleFill } from "react-icons/bs";
+import { poppins } from "../lib/utils/font";
+import Loader from "../lib/components/Loader";
+
+const Form = () => {
+  const params = useSearchParams()!;
+  const session = useSession();
+  const router = useRouter();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState<string | null>("");
+  const [success, setSuccess] = useState<string | null>("");
+
+  useEffect(() => {
+    setError(params.get("error"));
+    setSuccess(params.get("success"));
+  }, [params]);
+
+  if (session.status === "loading") {
+    return <Loader />;
+  }
+
+  if (session.status === "authenticated") {
+    router?.push("/my/dashboard");
+  }
+
+  const handleChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = event.target;
+    setForm((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { email, password } = form;
+    signIn("credentials", {
+      email,
+      password,
+    });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={`${styles.form_container} flex justify-center items-center flex-col`}
+    >
+      <h2
+        className={`leading-[1.15] mt-12 mx-auto w-full  pl-2 sm:pl-0 text-xl my-6 sm:text-2xl font-semibold font-sans ${poppins.variable}`}
+      >
+        Log In
+      </h2>
+
+      <fieldset className="w-full mx-4 flex justify-center items-center flex-col">
+        <label
+          className="w-full pl-2 sm:pl-0 "
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          name="email"
+          id="email"
+          type="email"
+          required
+          onChange={handleChange}
+          value={form.email}
+          className=" w-[95%]  sm:pl-0 mx-4 sm:mx-0 border-solid border-[1px] border-[#EAECEF]"
+        />
+      </fieldset>
+      <fieldset className="w-full mx-4 mt-12 flex justify-center items-center flex-col">
+        <label
+          className="w-full pl-2 sm:pl-0 "
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          name="password"
+          id="password"
+          type="password"
+          onChange={handleChange}
+          value={form.password}
+          required
+          className=" w-[95%]  sm:pl-0 mx-4 sm:mx-0 border-solid border-[1px] border-[#EAECEF]"
+        />
+      </fieldset>
+      <div className={`flex flex-col justify-center w-full items-center mx-4`}>
+        <button
+          type="submit"
+          className="w-full flex justify-center items-center"
+        >
+          <span className="text-center flex-1   mt-6 bg-[#2c6e49] hover:bg-lightColor hover:font-semibold rounded-md p-[1rem] px-4 mx-2  text-white cursor-pointer">
+            Log in
+          </span>
+        </button>
+        <p
+          className={`py-6  text-[#707a8a] text-center ${styles.login_continue}`}
+        >
+          <span className="mr-1 "> Or</span>
+        </p>
+      </div>
+      <div className="flex w-full justify-center px-2 text-lg items-center">
+        <GoogleButton />
+      </div>
+      <div className="flex w-full justify-center px-2 text-lg items-center">
+        <FacebookButton />
+      </div>
+
+      {error && (
+        <p className="text-red-700 w-full text-left uppercase tracking-wider text-sm font-semibold my-2 ">
+          {error}
+        </p>
+      )}
+
+      <div className="py-4 px-2 w-full">
+        <p>
+          <Link
+            href="/register"
+            className="text-lightColor hover:text-primaryColor hover:underline"
+          >
+            {" "}
+            Create a GamerSwap Account
+            <BsArrowRightCircleFill />
+          </Link>
+        </p>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/app/register/Form.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import styles from "./Form.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const Form = () => {
+  const router = useRouter();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+  const [message, setMessage] = useState<null | string>(null);
+
+  const handleChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = event.target;
+    setForm((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { name, email, password } = form;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+      res.status === 201 &&
+        router.push("/login?success=Account has been created");
+    } catch (err: any) {
+      setMessage(err);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      autoComplete="off"
+      className={`${styles.form_container} mt-10 flex justify-center items-center flex-col`}
+    >
+      <fieldset className="w-full mx-4 flex justify-center items-center flex-col">
+        <div className="w-full px-2">
+          <label
+            htmlFor="name"
+            className="text-sm"
+          >
+            Full Name
+          </label>
+          <input
+            name="name"
+            id="name"
+            type="text"
+            required
+            onChange={handleChange}
+            autoComplete="false"
+            value={form.name}
+            className="p-3 w-full border-solid border-[1px] border-[#EAECEF]"
+          />
+        </div>
+        <div className="w-full px-2">
+          <label
+            htmlFor="email"
+            className="text-sm"
+          >
+            Email
+          </label>
+          <input
+            name="email"
+            id="email"
+            type="email"
+            required
+            onChange={handleChange}
+            autoComplete="off"
+            value={form.email}
+            className="p-3 w-full border-solid border-[1px] border-[#EAECEF]"
+          />
+        </div>
+
+        <div className="w-full px-2">
+          <label
+            htmlFor="password"
+            className="text-sm"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            required
+            autoComplete="new-password"
+            id="password"
+            onChange={handleChange}
+            value={form.password}
+            className="p-3 w-full border-solid border-[1px] border-[#EAECEF]"
+          />
+        </div>
+      </fieldset>
+      <div className="flex flex-col w-full items-center px-2">
+        <p className="w-full text-left">
+          <Link
+            href="/login"
+            className="text-lightColor hover:text-primaryColor hover:underline"
+          >
+            {" "}
+            Login with an existing account
+          </Link>
+        </p>
+        {message && <p className="text-red-700 my-2 text-xl">{message}</p>}
+        <button
+          type="submit"
+          className="text-center my-12 flex-1 w-full bg-[#2c6e49] hover:bg-lightColor hover:font-semibold rounded-md p-[1rem] px-4 mx-2  text-white cursor-pointer"
+        >
+          Register
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
