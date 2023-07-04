9a4af9c6cd2811f17f35d67ffc0c8a8bae80fef1
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-07-04 10:29:24 +0100

    created gamer swap footer

--- src/app/lib/components/Layout/Footer.tsx
+++ src/app/lib/components/Layout/Footer.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-
 const Footer = () => {
   return (
-    <footer>
-      <p> Footer</p>
+    <footer className="pt-12 mt-6 max-w-7xl mx-auto">
+      <p className="text-center py-12 text-sm mx-4 xl:mx-0 text-[#707a8a] border-t-[1px] border-solid ">
+        Gamer Swap &#169; 2023
+      </p>
     </footer>
   );
 };
