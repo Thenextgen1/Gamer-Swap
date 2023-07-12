85062b442c594e51c404613883ff56684aa4cc36
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-07-12 03:16:48 +0100

    linked email service providers

--- frontend/src/app/lib/utils/auth.ts
+++ frontend/src/app/lib/utils/auth.ts
@@ -55,10 +55,12 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID!,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET!,
